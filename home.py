67b03def848b74fcf97cf7e7afaa6d9c642a523e
jayozer <jay.ozer@doma.com>
2023-04-02 12:45:28 -0700

    validate.py works. 0_enterprise.py works.

--- home.py
+++ home.py
@@ -4,16 +4,13 @@ from streamlit_extras.switch_page_button import switch_page
 st.set_page_config(layout="centered", page_title="Data Editor", page_icon="🧮")
 
 # Define the authorized usernames and passwords
-authorized_users = {"enterprise_user": "password1", "agency_user": "password2"}
+AUTHORIZED_USERS = {"enterprise_user": "password1"}
 
 def authenticate(username, password):
     """
     Authenticates the user by checking if the username and password match the authorized users.
     """    
-    if username in authorized_users and password == authorized_users[username]:
-        return True
-    else:
-        return False
+    return username in AUTHORIZED_USERS and password == AUTHORIZED_USERS[username]
 
 def login():
     """
@@ -38,23 +35,21 @@ if login():
 
     # Show pages once user is authenticated
     switch_page(pages)
-
+    
     @st.cache
     def icon(emoji: str):
         """Shows an emoji as a Notion-style page icon."""
         st.write(
             f'<span style="font-size: 78px; line-height: 1">{emoji}</span>',
-            unsafe_allow_html=True,
+            unsafe_allow_html=True
         )
 
     icon(":partying_face:")
     st.title("Streamable - Data Input @ DOMA")
-
     st.markdown(
         "Streamable looks just like a dataframe but it's editable! Users can click on"
         " cells and edit them. The lists are used in Associate Scorecard Dashboards"
     )
-
     st.sidebar.markdown(
         """
         Read more ...
@@ -62,9 +57,6 @@ if login():
     )
 
     # Conditionally show pages based on authentication status
-    if authenticate("username", "password"):
-        for page in pages:
-            if page == "enterprise":
-                switch_page(page)
-            elif page == "agency":
-                switch_page(page)
+    for page in pages:
+        if authenticate("username", "password"):
+            switch_page(page)
